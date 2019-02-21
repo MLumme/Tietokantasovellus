5ae52cca2adbd1b2670dce1292c31d94bdd5613e
MLumme <markusjuhani.lumme@gmail.com>
2019-02-21 22:12:34 +0200

    Fixed a mistake where threads date_edited was set to update on, well, update, should have been messages date_edited.

--- application/forum/forum_models.py
+++ application/forum/forum_models.py
@@ -11,7 +11,7 @@ class Thread(db.Model):
     __tablename__ = "thread"
     id = db.Column(db.Integer, primary_key = True)
     date_posted = db.Column(db.DateTime, default = db.func.current_timestamp())
-    date_edited = db.Column(db.DateTime, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
+    date_edited = db.Column(db.DateTime, default = db.func.current_timestamp())
     title = db.Column(db.String(200), nullable = False)
     messages = db.relationship("Message", cascade="all, delete-orphan")
     user_id = db.Column(db.Integer, db.ForeignKey('account.id'))
@@ -26,7 +26,7 @@ class Message(db.Model):
     __tablename__ = "message"
     id = db.Column(db.Integer, primary_key = True)
     date_posted = db.Column(db.DateTime, default = db.func.current_timestamp())
-    date_edited = db.Column(db.DateTime, default = db.func.current_timestamp())
+    date_edited = db.Column(db.DateTime, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
     content = db.Column(db.String(1000), nullable = False)
     thread_id = db.Column(db.Integer,db.ForeignKey('thread.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('account.id'))
